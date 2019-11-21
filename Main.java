910ee038ef2c61768ccc75a878bdf6595e3ccb8b
Sumit Khinchi <creativekhinchi@gmail.com>
2019-11-21 05:47:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class LinkedList
+{
+  Node head;
+  static class Node
+  {
+    
+  }
+}
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
