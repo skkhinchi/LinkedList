d46b9646cd4b097e149d7329f19c910da0c7245b
Sumit Khinchi <creativekhinchi@gmail.com>
2019-11-21 09:27:03 +0000

    Print Nodes

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 class LinkedList
 {
   Node head;
@@ -11,15 +12,16 @@ class LinkedList
       next=null;
     }
   }
+
+  //insert an element.
   public static LinkedList insert(LinkedList list,int data)
   {
     Node new_node = new Node(data);
     new_node.next=null;
 
     if(list.head==null)
-    { 
+    {
       list.head=new_node;
-
     }
     else
     {
@@ -28,16 +30,43 @@ class LinkedList
       {
         last=last.next;
       }
-    last.next=new_node;
+      last.next=new_node;
 
     }
     return list;
   }
+
+  public static void printList(LinkedList list)
+  {
+    Node currNode=list.head;
+    System.out.println("Linked List : ");
+
+    while(currNode!=null)
+    {
+      System.out.print(currNode.data + " ");
+      currNode=currNode.next;
+    } 
+
+  }
 }
 
+//--------------------------------------
+public class Main
+{
 
-class Main {
   public static void main(String[] args) {
+    
     System.out.println("Hello world!");
+    LinkedList list =new LinkedList();
+    
+    list=LinkedList.insert(list,1);
+    list=LinkedList.insert(list,2);
+    list=LinkedList.insert(list,3);
+    list=LinkedList.insert(list,4);
+    list=LinkedList.insert(list,5);
+    list=LinkedList.insert(list,6);
+    list=LinkedList.insert(list,7);
+
+    LinkedList.printList(list);
   }
 }
